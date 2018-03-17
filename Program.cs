4f9f55a12037b8b896ae1bc643ea3d10973c3aa0
amichai <ami.dan.man@gmail.com>
2018-03-17 16:56:11 +0200

    17.3.18

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Targil2 t = new Targil2();
+            
+            /*Console.WriteLine("hey honey, enter string");
+            string txt = Console.ReadLine();
+            
+            Console.WriteLine("hey sweety. which inner string would you like to replace?");
+            string subtext = Console.ReadLine();
+
+            Console.WriteLine("you're amazing! now enter the new text");
+            string replace = Console.ReadLine();
+
+            Console.WriteLine(t.replace(txt, subtext, replace));
+            
+            Console.WriteLine("ENter string and I'll reverse it");
+            string str = Console.ReadLine();   
+            Console.WriteLine(t.reverse(str));*/
+
+            
+            Console.WriteLine("enter text mathafacka");
+            string text = Console.ReadLine();
+          
+            
+            Console.WriteLine("enter char to remove");
+            char ch = Char.Parse(Console.ReadLine());
+            
+            Console.WriteLine(t.removalItems(text,ch));
+        }
+    }
+}
---
+++ Targil2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    public class Targil2
+    {
+        public string replace(string txt, string substring, string replace)
+        {
+            string newStriong = txt.Replace(substring, replace);        //replace method in string, does not change original string. it returns a new one.
+
+            return newStriong;
+        }
+
+        public string reverse(string s)
+        {
+            char[] array = s.ToCharArray();  //עכשיו הפכתי את הסטרינג למערך תווים
+            Array.Reverse(array);
+            string newString = new string(array); //הפכנו את המערך לסטרינג
+            return newString;
+        }
+        
+        public string removalItems(string str, char ch)
+        {
+            //str.Replace(oldString, newString);    //מחליף את הolostring עם newstring
+            
+            
+            ch.
+            return str.Replace(ch.ToString(),"");
+        }
+        
+        public string getSkip(string text, int skipSize)
+        {
+            return text.Substring(skipSize);
+        }
+    }
+}
